97b8fdae52ccf5552fbbb7cff36b81e58fb56c54
Annemarii <annemarii.hunt@gmail.com>
2019-03-29 14:52:50 +0200

    Menu pisiparandus

--- Scenes/Menu.gd
+++ Scenes/Menu.gd
@@ -4,15 +4,15 @@ export(String, FILE, "*.tscn") var change_level
 export(String) var target_spawn_group
 
 func _ready():
-	$MarginContainer/VBoxContainer/VBoxContainer/TextureButton.grab_focus()
+	$VBoxContainer2/TextureButton.grab_focus()
 	
 	
 	
 func _physics_process(delta):
-	if $MarginContainer/VBoxContainer/VBoxContainer/TextureButton.is_hovered()==true:
-		$MarginContainer/VBoxContainer/VBoxContainer/TextureButton.grab_focus()
-	if $MarginContainer/VBoxContainer/VBoxContainer/TextureButton2.is_hovered()==true:
-		$MarginContainer/VBoxContainer/VBoxContainer/TextureButton2.grab_focus()
+	if $VBoxContainer2/TextureButton.is_hovered()==true:
+		$VBoxContainer2/TextureButton.grab_focus()
+	if $VBoxContainer2/TextureButton2.is_hovered()==true:
+		$VBoxContainer2/TextureButton2.grab_focus()
 		
 	
 
