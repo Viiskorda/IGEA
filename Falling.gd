565dfbb00ac46f9b81e8a369468224905572ddab
Viiskorda <annemarii.hunt@gmail.com>
2019-04-02 16:47:47 +0300

    Nüüd enam ei ole stseeni kokku jooksmist

---
+++ Falling.gd
@@ -0,0 +1,26 @@
+#LevelComplete.gd
+
+extends Area2D
+
+export(String, FILE, "*.tscn") var change_level
+export(String) var target_spawn_group
+
+const SAVE_PATH ={}
+
+func save_game():
+	pass
+
+
+func load_game():
+	pass
+
+func _physics_process(_delta):
+	var bodies= get_overlapping_bodies()
+	for body in bodies:
+		if body.name=="Chara":
+			#get_tree().change_scene(change_level)
+			Global.goto_scene(change_level,target_spawn_group)
+
+
+
+
