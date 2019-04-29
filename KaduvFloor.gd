5e72af0764109034c83ac98f66b44f514bdb8487
Viiskorda <annemarii.hunt@gmail.com>
2019-04-29 19:13:49 +0300

    Koopa suu

--- KaduvFloor.gd
+++ KaduvFloor.gd
@@ -1,6 +1,13 @@
 extends StaticBody2D
 
-
-
 func _on_Area2D_body_entered(body):
-	$CollisionShape2D.call_deferred("set_disabled", false)
+	$CollisionShape2D.call_deferred("set_disabled", false)	
+
+func _process(delta):
+	if Global.bossHealth <= 0:
+		$CollisionShape2D.call_deferred("set_disabled", true)
+		
+	elif Global.enemy1isalive==true and Global.enemy3isalive==true and Global.enemy4isalive==true and Global.enemy5isalive==true and Global.enemy6isalive==true:
+		$CollisionShape2D.call_deferred("set_disabled", false)
+
+	
