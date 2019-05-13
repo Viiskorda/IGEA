fed6afd85004fe233618b980e34d7b3cf0cbe27b
Annemarii Hunt <annemarii.hunt@gmail.com>
2019-05-13 21:06:01 +0300

    Stseeni vahetusel 2sekundiline viide ja fadeout

--- Scripts/LevelComplete.gd
+++ Scripts/LevelComplete.gd
@@ -5,21 +5,33 @@ export(String, FILE, "*.tscn") var change_level
 export(String) var target_spawn_group
 
 
-func load_game():
+func _ready():
+	#$FadeOut.start()
+	$Sprite.modulate.a=0
 	pass
+
 
 func _physics_process(_delta):
 	var bodies= get_overlapping_bodies()
 	for body in bodies:
 		if body.name=="Chara":
 			#get_tree().change_scene(change_level)
-			Global.goto_scene(change_level,target_spawn_group)
+			$Sprite.modulate.a=0
+			$Timer.start()
+			$Modulate.start()
+
+			
 
 
 
+func _on_Timer_timeout():
+
+	Global.goto_scene(change_level,target_spawn_group)
 
 
+func _on_Modulate_timeout():
+	$Sprite.modulate.a+=.1
 
 
-
-
+func _on_FadeOut_timeout():
+	$Sprite.modulate.a-=.1
